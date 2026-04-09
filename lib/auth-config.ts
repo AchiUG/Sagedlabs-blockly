abc3cd2e1e87c378ef57c9023a2797fbf63d0eb6
Adolph97 <jakeorb.ji@gmail.com>
2026-04-09 14:52:52 +0100

    Config: Upgrade of repo for production use case(vercel)

--- lib/auth-config.ts
+++ lib/auth-config.ts
@@ -8,6 +8,7 @@ import { prisma } from './db';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
